cea48ede32df35cf132770fa8803f5b3f0c83491
Tincho Sesto <sesto@outlook.com>
2020-10-14 08:54:13 -0300

    lambda called by services (s3 & cognito)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { Storage } from 'aws-amplify'
+import { v4 as uuid } from 'uuid'
+import './App.css'
 
 function App() {
+  const [images, setImages] = useState([])
+  useEffect(() => {
+    fetchImages()
+  }, [])
+  async function onChange(e) {
+    /* When a file is uploaded, create a unique name and save it using
+       the Storage API */
+    const file = e.target.files[0];
+    const filetype = file.name.split('.')[file.name.split.length - 1]
+    await Storage.put(`${uuid()}.${filetype}`, file)
+    /* Once the file is uploaded, fetch the list of images */
+    fetchImages()
+  }
+  async function fetchImages() {
+    /* This function fetches the list of image keys from your S3 bucket */
+    const files = await Storage.list('')
+    /* Once we have the image keys, the images must be signed in order
+       for them to be displayed */
+    const signedFiles = await Promise.all(files.map(async file => {
+      /* To sign the images, we map over the image key array and get a
+         signed url for each image */
+      const signedFile = await Storage.get(file.key)
+      return signedFile
+    }))
+    setImages(signedFiles)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type="file"
+          onChange={onChange}
+        />
+        {
+          images.map(image => (
+            <img
+              src={image}
+              key={image}
+              style={{ width: 500 }}
+            />
+          ))
+        }
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
